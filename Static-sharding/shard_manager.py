c11c511c85adca38016c75a391cb4efd8eed2b73
OmarDesoky <=>
2025-05-19 19:29:05 +0300

    change hash fn to fnv1_hash

--- Static-sharding/shard_manager.py
+++ Static-sharding/shard_manager.py
@@ -6,10 +6,18 @@ import api_pb2_grpc
 NUM_GROUPS = 2
 GROUP_PORTS = [40051, 50051]
 
+def fnv1_hash(string):
+    fnv_prime = 0x01000193
+    hash_val = 0x811c9dc5
+    for char in string:
+        hash_val = (hash_val * fnv_prime) % (1 << 32)
+        hash_val = hash_val ^ ord(char)
+    return hash_val
+
 class ShardManagerServicer(api_pb2_grpc.ShardManagerServicer):
     def GetShardIndex(self, request, context):
         key = request.key
-        shard_index = hash(key) % NUM_GROUPS
+        shard_index = fnv1_hash(key) % NUM_GROUPS
         port = GROUP_PORTS[shard_index]
         return api_pb2.ShardResponse(port=port)
 
